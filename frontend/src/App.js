ab8cf7ef6df6c57249788293423ff3cfd6a67c46
tomual <tomual@users.noreply.github.com>
2017-12-05 18:14:18 -0600

    Change URL without refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
+
+import Home from './components/Home';
+import Works from './components/Works';
+import Pricing from './components/Pricing';
+import About from './components/About';
+import Contact from './components/Contact';
 
 class App extends Component {
     constructor() {
@@ -32,9 +39,25 @@ class App extends Component {
             message = this.state.message;
         }
         return (
-            <div className="App">
-                {message}
-            </div>
+            <Router>
+                <div className="App">
+                    Database message: {message}
+
+                    <ul>
+                        <li><Link to="">Home</Link></li>
+                        <li><Link to="works">Works</Link></li>
+                        <li><Link to="pricing">Pricing</Link></li>
+                        <li><Link to="about">About</Link></li>
+                        <li><Link to="contact">Contact</Link></li>
+                    </ul>
+                    
+                    <Route exact path="/" component={Home} />
+                    <Route path="/works" component={Works} />
+                    <Route path="/pricing" component={Pricing} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                </div>
+            </Router>
         );
     }
 }
---
+++ frontend/src/components/Works.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Works extends Component {
+    render() {
+        return (
+            <div>
+                Works
+            </div>
+        );
+    }
+}
+
+export default Works;
